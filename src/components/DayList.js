95be137ef572b0a37ba41aa3ec7fb40fc935ec58
Kevinliao0857 <kevinliao0857@gmail.com>
2021-03-15 18:53:12 -0700

    day 1 react

---
+++ src/components/DayList.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import DayListItem from "components/DayListItem";
+const classNames = require("classnames")
+
+
+export default function DayList(props){
+  console.log({props})
+  const days = props.days.map(day => {
+    return(
+      <ul>     
+        <DayListItem
+          name={day.name} 
+          spots={day.spots} 
+          selected={day.name === props.day}
+          setDay={props.setDay}  
+          />
+        </ul>
+    )
+  });
+  return days;
+}
---
+++ src/components/DayListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import "components/DayListItem.scss";
+const classNames = require("classnames")
+
+
+export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots,
+  });
+
+ function formatSpots(spotsOpen) {
+    if (spotsOpen === 0) {
+        return "no spots remaining";
+    } else if (spotsOpen === 1) {
+        return "1 spot remaining";
+    } else {
+        return `${spotsOpen} spots remaining`;
+    }
+  };
+
+  return (
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
