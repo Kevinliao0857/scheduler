ab1a47566bef392cd227b8e2cb6d734f1391ac89
Kevinliao0857 <kevinliao0857@gmail.com>
2021-03-17 17:39:14 -0700

    retrieving appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,48 +6,48 @@ import "components/Application.scss";
 
 const axios = require('axios').default;
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Bobby Lee",
-      interviewer: {
-        id: 2,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png",
-      },
-    },
-  },
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "John Smith",
-      interviewer: {
-        id: 3,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/T2WwVfS.png",
-      },
-    },
-  },
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Bobby Lee",
+//       interviewer: {
+//         id: 2,
+//         name: "Tori Malcolm",
+//         avatar: "https://i.imgur.com/Nmx0Qxo.png",
+//       },
+//     },
+//   },
+//   {
+//     id: 3,
+//     time: "2pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       },
+//     },
+//   },
+//   {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "John Smith",
+//       interviewer: {
+//         id: 3,
+//         name: "Mildred Nazir",
+//         avatar: "https://i.imgur.com/T2WwVfS.png",
+//       },
+//     },
+//   },
+// ];
 
 // const days = [
 //   {
@@ -70,16 +70,45 @@ const appointments = [
 
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
-
-  useEffect(() => {
-    axios.get("/api/days").then(response => {
-      setDays([...response.data]);
-    });
-  }, [])
-
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+  const dailyAppointments = [];
   
+  const setDay = day => setState({...state, day});
+  // const setDays = (days) => setState(prev => ({ ...prev, days }));
+  
+
+  // useEffect(() => {
+  //   axios.get("/api/days").then(response => {
+  //     setDays([...response.data]);
+  //   });
+  // }, [])
+
+  // useEffect(() => {
+  //   axios.get("/api/appointments").then(response => {
+  //     console.log(response.data);
+  //   });
+  // }, [])
+
+  Promise.all([
+    Promise.resolve("first"),
+    Promise.resolve("second"),
+    Promise.resolve("third"),
+  ]).then((all) => {
+    console.log(all[0]); // first
+    console.log(all[1]); // second
+    console.log(all[2]); // third
+  
+    const [first, second, third] = all;
+  
+    console.log(all)
+    console.log(first, second, third);
+  });
 
   return (
     <main className="layout">
@@ -91,7 +120,10 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList 
+          days={state.days} 
+          day={state.day} 
+          setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -102,7 +134,7 @@ export default function Application(props) {
       <section className="schedule">
       
 
-      {appointments.map(appointment => 
+      {dailyAppointments.map(appointment => 
 
           <Appointment 
           key={appointment.id}
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,20 @@
 
 
-export default function getAppointmentsForDay(state, day) {
-  const filteredDays = state.days.filter(day => day.day === day)
+export function getAppointmentsForDay(state, day) {
+  const filteredDays = state.days.find(days => days.name === day)
+  if (!filteredDays) {
+    return []
+  }
+
+  const appId = filteredDays.appointments
+  const appForDay = [];
+
+  for(const id in state.appointments) {
+    if(appId.includes(Number(id))) {
+      appForDay.push(state.appointments[id])
+    }
+  }
+return appForDay
+
 };
+  
