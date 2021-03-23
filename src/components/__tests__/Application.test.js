b01ed8ca4db1124680920a8f4706e9e7e85ac25c
Kevinliao0857 <kevinliao0857@gmail.com>
2021-03-22 20:25:23 -0700

    the next tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, getAllByTestId, getByAltText, getByPlaceholderText, prettyDOM } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, getAllByTestId, getByAltText, getByPlaceholderText, queryByText, prettyDOM } from "@testing-library/react";
 
 
 import Application from "components/Application";
+
+
 
 afterEach(cleanup);
 
@@ -27,7 +29,7 @@ describe("Application", () => {
   });
   
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-    const { container, dubug } = render(<Application />);
+    const { container, debug } = render(<Application />);
   
     await waitForElement(() => getByText(container, "Archie Cohen"));
   
@@ -39,14 +41,34 @@ describe("Application", () => {
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" }
     });
-    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
   
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     fireEvent.click(getByText(appointment, "Save"));
   
-    console.log(prettyDOM(appointment));
-
     expect(getByText(appointment, "Saving")).toBeInTheDocument();
+  
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+  
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+  
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
+  
+  
+
+
+  // it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+  //   // 1. Render the Application.
+  //   // 2. Wait until the text "Archie Cohen" is displayed.
+  //   // 3. Click the "Delete" button on the booked appointment.
+  //   // 4. Check that the confirmation message is shown.
+  //   // 5. Click the "Confirm" button on the confirmation.
+  //   // 6. Check that the element with the text "Deleting" is displayed.
+  //   // 7. Wait until the element with the "Add" button is displayed.
+  //   // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+  // });
   
   
 
