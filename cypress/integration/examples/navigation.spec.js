280350e2fbbbd456735635d50adfef93fd3ec9d9
Kevinliao0857 <kevinliao0857@gmail.com>
2021-03-23 21:07:34 -0700

    cypress basic nav testing

--- cypress/integration/examples/navigation.spec.js
+++ cypress/integration/examples/navigation.spec.js
@@ -4,6 +4,11 @@ describe("Navigation", () => {
   });
   it("should navigate to Tuesday", () => {
     cy.visit("/");
+  
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected")
   });
+  
   
 });
